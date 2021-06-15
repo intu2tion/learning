03c198c87a062a4dfffc62c0f1ced12c5b3e530d
intu2tion <85495658+intu2tion@users.noreply.github.com>
2021-06-16 00:49:05 +0900

    html+css+js

---
+++ colors.js
@@ -0,0 +1,46 @@
+var links = 
+{
+    setcolor:function (color)
+    {
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < alist.length)
+        //     {
+        //     console.log(alist[i]);
+        //     alist[i].style.color = color;
+        //     i=i+1;
+        //     }
+        $('a').css('color', color);
+    }
+}
+var body = 
+{
+    setcolor:function (color)
+    {
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
+    },
+    setbackgroundcolor:function (color)
+    {
+        // document.querySelector('body').style.backgroundColor=color;
+        $('body').css('backgroundColor', color);
+    }
+}
+function nightdayhandler(self)
+{
+  var target = document.querySelector('body');
+  if(self.value === 'night')
+  { 
+   body.setbackgroundcolor('black');
+   body.setcolor('white');
+   self.value = 'day';
+   links.setcolor('powderblue');
+  } 
+ else 
+  {
+   body.setbackgroundcolor('white');
+   body.setcolor('black');
+   self.value = 'night';
+   links.setcolor('black');
+  }
+}
